95fb47e214b4724fa93969bbd02ce0772aaddb12
Joe Gregoria <jgregoria@gmail.com>
2017-05-16 12:35:14 -0400

    comments are good.

--- src/ArabicNumberToRomanNumeral.cs
+++ src/ArabicNumberToRomanNumeral.cs
@@ -3,15 +3,26 @@ using System.Text;
 
 namespace RomanNumerals
 {
+    /// <summary>
+    ///  The Romans wrote their numbers using letters; specifically the letters 'I' meaning '1', 'V' meaning '5', 'X' meaning '10', 
+    /// 'L' meaning '50', 'C' meaning '100', 'D' meaning '500', and 'M' meaning '1000'. 
+    /// 
+    /// There were certain rules that the numerals followed which should be observed:
+    ///
+    /// The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row. The symbols 'V', 'L', and 'D' can never be repeated. 
+    /// The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values('IV' and 'IX', 'XL' and 'XC', 'CD' and 'CM'). 
+    /// Only one subtraction can be made per numeral('XC' is allowed, 'XXC' is not). The '5' symbols('V', 'L', and 'D') can never be subtracted.
+    /// </summary>
     public class ArabicNumberToRomanNumeral
     {
-        // The Romans wrote their numbers using letters; specifically the letters 'I' meaning '1', 'V' meaning '5', 'X' meaning '10', 
-        // 'L' meaning '50', 'C' meaning '100', 'D' meaning '500', and 'M' meaning '1000'. There were certain rules that the numerals followed which should be observed.
-        //
-        // The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row. The symbols 'V', 'L', and 'D' can never be repeated. 
-        // The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values('IV' and 'IX', 'XL' and 'XC', 'CD' and 'CM'). 
-        // Only one subtraction can be made per numeral('XC' is allowed, 'XXC' is not). The '5' symbols('V', 'L', and 'D') can never be subtracted.
-        private SortedDictionary<int, string> ArabicToRomanMap = new SortedDictionary<int, string>(new DecendingIntegerComparer())
+       
+        /// <summary>
+        /// A mapping of special arabic numbers to their roman numeral counterpart. This map is used to drive the creation of the roman from the arabic.
+        /// The Keys are arabic integers were something interesting happens; the Values are their roman rumeral equivalant.
+        /// We include values like "CM", "CD", "XC", "XL", "IX", and "IV" to keep the algorithm cleaner.
+        /// The algorithm depends on the keys to be in decending order.
+        /// </summary>
+        private SortedDictionary<int, string> ArabicToRomanDecendingSpecialValues = new SortedDictionary<int, string>(new DecendingIntegerComparer())
         {
             {1000, "M"  },
             { 900, "CM" },  // special case
@@ -28,22 +39,29 @@ namespace RomanNumerals
             {   1, "I"  }
         };
 
+        /// <summary>
+        /// Convert an integer in to its roman numeral equivalant.
+        /// </summary>
+        /// <param name="arabicValue">an integer</param>
+        /// <returns>a string containing the roman numeral value. 
+        /// Will be empty for values that cannot be expressed in roman numerals (e.g., zero, negative numbers)</returns>
         public string Convert(int arabicValue)
         {
             int valueRemaining = arabicValue;
-            StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder returnStringBuilder = new StringBuilder();
 
-            foreach (var entry in ArabicToRomanMap)
+            foreach (var specialValueEntry in ArabicToRomanDecendingSpecialValues)
             {
-                while (valueRemaining >= entry.Key)
+                // we depend on the keys to be decending for this algorithm to work
+                while (valueRemaining >= specialValueEntry.Key)
                 {
-                    stringBuilder.Append(entry.Value);
-                    valueRemaining = valueRemaining - entry.Key;
+                    returnStringBuilder.Append(specialValueEntry.Value);
+                    valueRemaining = valueRemaining - specialValueEntry.Key;
                     if (valueRemaining == 0) break;
                 }
             }
 
-            return stringBuilder.ToString();
+            return returnStringBuilder.ToString();
         }
     }
 }
