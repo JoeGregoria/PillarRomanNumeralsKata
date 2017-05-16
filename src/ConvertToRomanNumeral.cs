dd28671e906faba3174245580e1103c7376a0fb3
Joe Gregoria <jgregoria@gmail.com>
2017-05-16 11:41:12 -0400

    Create a command line interface for doing the conversion.

---
+++ src/ConvertToRomanNumeral.cs
@@ -0,0 +1,28 @@
+﻿using RomanNumerals;
+using System;
+
+namespace PillarRomanNumeralsKata
+{
+    class ConvertToRomanNumeral
+    {
+        public static int Main(string[] args)
+        {
+            if (args == null)
+            {
+                Console.Error.WriteLine("usage: ConvertToRomanNumeral <number>");
+                return 0;
+            }
+
+            int value;
+            if (!int.TryParse(args[0], out value))
+            {
+                Console.Error.Write("cannot parse input; must be an integer");
+                return 0;
+            }
+
+            ArabicNumberToRomanNumeral arabicNumberToRomanNumeral = new ArabicNumberToRomanNumeral();
+            Console.WriteLine(arabicNumberToRomanNumeral.Convert(value));
+            return 1;
+        }
+    }
+}
