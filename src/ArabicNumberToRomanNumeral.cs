f58ab02b581bad20bfa47555f0808d47eb9e132b
Joe Gregoria <jgregoria@gmail.com>
2017-05-16 11:18:24 -0400

    Handle 5 and 10.

--- src/ArabicNumberToRomanNumeral.cs
+++ src/ArabicNumberToRomanNumeral.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,40 @@ namespace RomanNumerals
 {
     public class ArabicNumberToRomanNumeral
     {
+        private SortedDictionary<int, string> ArabicToRomanDictionary = new SortedDictionary<int, string>(new IntComparerHighestFirst())
+        {
+            { 10, "X" },
+            {  5, "V" },
+            {  1, "I" }
+        };
+
         public string Convert(int value)
         {
+            int valueRemaining = value;
             StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < value; i++)
+
+            foreach (var entry in ArabicToRomanDictionary)
             {
-                stringBuilder.Append("I");
+                while (valueRemaining >= entry.Key)
+                {
+                    stringBuilder.Append(entry.Value);
+                    valueRemaining = valueRemaining - entry.Key;
+                    if (valueRemaining == 0) break;
+                }
             }
+
             return stringBuilder.ToString();
+        }
+    }
+
+    public class IntComparerHighestFirst : IComparer<int>
+    {
+        public int Compare(int left, int right)
+        {
+            if (left == right) return 0;
+            if (left < right) return 1;
+            if (left > right) return -1;
+            throw new ArgumentException("what the heck?");
         }
     }
 }
--- test/ArabicNumberToRomanNumeralTests.cs
+++ test/ArabicNumberToRomanNumeralTests.cs
@@ -19,5 +19,26 @@ namespace RomanNumeralsTests
             ArabicNumberToRomanNumeral arabicNumberToRomanNumeral = new ArabicNumberToRomanNumeral();
             Assert.Equal("III", arabicNumberToRomanNumeral.Convert(3));
         }
+
+        [Fact]
+        public void Given5ShouldReturnV()
+        {
+            ArabicNumberToRomanNumeral arabicNumberToRomanNumeral = new ArabicNumberToRomanNumeral();
+            Assert.Equal("V", arabicNumberToRomanNumeral.Convert(5));
+        }
+
+        [Fact]
+        public void Given10ShouldReturnX()
+        {
+            ArabicNumberToRomanNumeral arabicNumberToRomanNumeral = new ArabicNumberToRomanNumeral();
+            Assert.Equal("X", arabicNumberToRomanNumeral.Convert(10));
+        }
+
+        //[Fact]
+        //public void Given9ShouldReturnIX()
+        //{
+        //    ArabicNumberToRomanNumeral arabicNumberToRomanNumeral = new ArabicNumberToRomanNumeral();
+        //    Assert.Equal("IX", arabicNumberToRomanNumeral.Convert(9));
+        //}
     }
 }
